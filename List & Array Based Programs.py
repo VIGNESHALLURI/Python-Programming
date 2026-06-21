638860f7e96fe22ececcef58c6f11976ff52df34
SAIVARMA01 <vigneshvarmaalluri@gmail.com>
2026-06-21 12:31:51 +0530

    List & Array programs

---
+++ List & Array Based Programs.py
@@ -0,0 +1,21 @@
+#take an array / list of 10 random numbers. print elements in reverse order. 
+#python students using for loop with ranges. java people should use a for loop. 
+arr = [10,20,30,40,50,60,70,80,90,100]
+for i in range(len(arr)-1,-1,-1):
+    print(arr[i], end=" ")
+#take an array / list of 10 random numbers. print alternate elements 0th, 2nd, 4th etc. 
+#Python students use for loop with ranges. java should use a for loop. 
+arr = [10,20,30,40,50,60,70,80,90,100]
+for i in range(0,len(arr),2):
+    print(arr[i], end=" ")
+#take array 1/list1 with some random numbers. copy the elements of that array / list into a second array2 / list2. 
+#take array 1/list1 with some random numbers. copy the elements of that array / list into a second array2 / list2 in reverse order 
+#take an array / list of 10 random numbers. write logic to count how many even nos are there and how many odd numbers are there. 
+#take an array / list of 10 random numbers. find big in that array / list. don't use the max() function. 
+#take an array / list of 10 random numbers. find even big and odd big in that array / list. don't use the max() function. 
+#take an array / list of 10 random numbers. copy even numbers into array2/list2. 
+# copy odd numbers into array3/list3. don't use copy() or other copying methods. 
+#take arr1/list1, arr2/list2 with some random numbers. take an empty arr3/list3 of relevant size. 
+#copy first arr1/list1 into arr3/list3. copy the next arr2/list2 elements into arr3/list3. 
+#take an arr / list with some numbers. print even nos present in the even positions. 
+#take 2 arrays / lists with some numbers. add both arrays / list into third array / list 
